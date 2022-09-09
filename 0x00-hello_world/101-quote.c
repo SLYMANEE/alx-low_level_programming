e099bae984da465c1a898bac4678b6386014ffcf
SLYMANEE <slyzamani@gmail.com>
2022-09-09 14:45:57 -0700

    dohn

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 1 (Success)
+ */
+
+int main(void)
+{
+	char *text = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	int le = strlen(text);
+
+	write(2, text, le);
+	return (1);
+}
