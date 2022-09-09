ebbf28dca4d9b3a8b24b6f0a6a3e1094f06975d8
SLYMANEE <slyzamani@gmail.com>
2022-09-09 14:26:16 -0700

    s

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: Prints "programming is like building a multilingual puzzle"
+ * Description: prints "Programming is like building a multilingual puzzle"
  *
  * Return: Always 0 (Success)
  *
@@ -12,10 +12,8 @@
 int main(void)
 
 {
-                 	puts("\"programming is like building a multilingual puzzle");
-
-	                        return (0);
-
+				puts("\"Programming is like building a multilingual puzzle");
+								return (0);
 
 }
 
